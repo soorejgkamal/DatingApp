9e77c337f775b263df0470c653649bf00ac2c760
SOORssEJ G KAssMAL <soorejgkamssal@outlook.com>
2019-12-03 13:20:25 -0500

    end of section

--- DatingApp.API/Program.cs
+++ DatingApp.API/Program.cs
@@ -16,7 +16,7 @@ namespace DatingApp.API
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
