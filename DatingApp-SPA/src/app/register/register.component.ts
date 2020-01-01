7d019609c06fb858456c153eb172bc9b547b5c73
SOORssEJ G KAssMAL <soorejgkamssal@outlook.com>
2020-01-01 16:29:21 -0500

    end of section 12

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input , EventEmitter, Output } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,20 +13,55 @@ import { AlertifyService } from '../_services/alertify.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  model: any = {};
+  user: User;
   @Output() cancelRegister = new EventEmitter();
-  constructor(private authservice: AuthService, private alertify: AlertifyService) { }
+  registerForm: FormGroup;
+  bsConfig:Partial<BsDatepickerConfig>;
+
+  constructor(private authservice: AuthService, private alertify: AlertifyService, 
+    private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+
+    this.bsConfig = {
+      containerClass: 'theme-red'
+    }
+    this.createRegisterForm();
+  }
+
+  createRegisterForm(){
+    this.registerForm =  this.fb.group({
+      gender : ['male'],
+      username : ['',Validators.required],
+      knownAs : ['',Validators.required],
+      dateOfBirth: ['',Validators.required],
+      city: ['',Validators.required],
+      country: ['',Validators.required],
+      password : ['',[Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmpassword : ['' , Validators.required]
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmpassword').value ? null : {mismatch: true};
   }
 
   register() {
-    this.authservice.register(this.model).subscribe(() => {
-      this.alertify.success('registration success');
-    }, error => {
-      console.log(error);
-      this.alertify.error(error);
-    });
+
+    if(this.registerForm.valid){
+      this.user= Object.assign({},this.registerForm.value);
+      this.authservice.register(this.user).subscribe(() => {
+        this.alertify.success('registration success');
+      }, error => {
+        console.log(error);
+        this.alertify.error(error);
+      },() => {
+        this.authservice.login(this.user).subscribe(()=>{
+           this.router.navigate(['/members']);
+        })
+      });
+
+    }
   }
   cancel() {
     this.cancelRegister.emit(false);
--- DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/Controllers/AuthController.cs
@@ -36,14 +36,10 @@ namespace DatingApp.API.Controllers
             if(await _repo.UserExists(userForRegisterDto.UserName))
                 return BadRequest("user name already taken");
 
-            var userToCreate=new User()
-            {
-                UserName=userForRegisterDto.UserName
-            };
-
+            var userToCreate=  _mapper.Map<User>(userForRegisterDto);
             var createdUser=await _repo.Register(userToCreate,userForRegisterDto.Password);
-
-            return StatusCode(201);
+            var userToReturn= _mapper.Map<UserForDetailedDto>(createdUser);
+            return CreatedAtRoute("GetUser",new {Controller = "Users",id= createdUser.Id },userToReturn);
         }
         [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
